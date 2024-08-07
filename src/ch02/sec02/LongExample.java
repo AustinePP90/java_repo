f2bb97d142ff7b75c896385d4ebb2ab9a6a61e6c
Austine <chucky891213@naver.com>
2024-08-07 17:33:25 +0900

    24-08-07 ch02.sec02 주석 추가

--- src/ch02/sec02/LongExample.java
+++ src/ch02/sec02/LongExample.java
@@ -5,7 +5,11 @@ public class LongExample {
 	public static void main(String[] args) {
 		long var1 = 10;
 		long var2 = 20L;
-		long var3 = 1000000000000L; // 컴파일러는 int로 간주하기 때문에 에러 발생(뒤에 L 써줌!, 1000000000000 -> 1000000000000L)
+		/*
+		 * 컴파일러는 정수 리터럴을 int 타입 값으로 간주하기 때문에 에러 발생!(뒤에 소문자 l이나 대문자L을 써서 long 타입 값임을
+		 * 컴파일러에게 알려 줘야 한다 , 1000000000000 -> 1000000000000L)
+		 */
+		long var3 = 100000000000l;
 		long var4 = 1000000000000L;
 
 		System.out.println(var1);
