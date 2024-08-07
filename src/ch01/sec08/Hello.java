648f9056174e0696891f83e56087e608ebd37ef7
Austine <chucky891213@naver.com>
2024-08-07 16:35:31 +0900

    24-08-07 ch01.sec08 주석 달기

--- src/ch01/sec08/Hello.java
+++ src/ch01/sec08/Hello.java
@@ -1,8 +1,9 @@
 package ch01.sec08;
 
 public class Hello {
-
+	// 프로그램 실행 진입점
 	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
 		System.out.println("Me Three! You can say that again! Java08");
 
 	}
