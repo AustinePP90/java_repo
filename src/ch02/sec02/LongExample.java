e93015a6a7e0106f6a931b4391051d248d963c9c
Austine <chucky891213@naver.com>
2024-08-07 16:24:46 +0900

    24-08-07 ch02.sec02 정수 실습(Long 타입)

---
+++ src/ch02/sec02/LongExample.java
@@ -0,0 +1,18 @@
+package ch02.sec02;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 20L;
+		long var3 = 1000000000000L; // 컴파일러는 int로 간주하기 때문에 에러 발생(뒤에 L 써줌!, 1000000000000 -> 1000000000000L)
+		long var4 = 1000000000000L;
+
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+
+	}
+
+}
