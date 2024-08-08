c9327213290db3542fcbee9002f30d18aa8daeea
Austine <chucky891213@naver.com>
2024-08-08 12:44:53 +0900

    24-08-08 ch02.sec04 코드, 주석 추가

--- src/ch02/sec04/FloatDoubleExample.java
+++ src/ch02/sec04/FloatDoubleExample.java
@@ -3,6 +3,12 @@ package ch02.sec04;
 public class FloatDoubleExample {
 
 	public static void main(String[] args) {
+		// 컴파일러는 실수 리터럴을 기본적으로 double 타입으로 해석, float 타입에 대입하고 싶다면 리터럴 뒤에 소문자 'f'나 대문자 'F'를 붙여야 함!
+		double varD = 3.14;
+		float varF = 3.14f; // float varF = 3.14F;
+		System.out.println(varD);
+		System.out.println(varF);
+		
 		// 정밀도 확인
 		float var1 = 0.1234567890123456789f;
 		double var2 = 0.1234567890123456789;
@@ -12,7 +18,7 @@ public class FloatDoubleExample {
 		// 10의 거듭제곱 리터럴
 		double var3 = 3e6;
 		float var4 = 3e6F;
-		double var5 = 2e-3;
+		double var5 = 2e-3; // 2.0 * 10의 -3승(-1000)
 		System.out.println("var3: " + var3);
 		System.out.println("var4: " + var4);
 		System.out.println("var5: " + var5);
